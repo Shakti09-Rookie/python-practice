d7e243a4f466bcc7611ae65f8d5f4163a9134969
Shakti09-Rookie <shaktidagar09@gmail.com>
2022-03-17 01:48:43 +0530

    stackc.py

--- dsa/stack.py
+++ dsa/stack.py
@@ -13,9 +13,17 @@ class stack:
     def peek(self):
         print(self.stack[-1])
 
+    def pop(self):
+        return self.stack.pop()
+
 if __name__ == "__main__":
     stack1 = stack()
     stack1.addinstack("5")
     stack1.addinstack("6")
+    stack1.addinstack("7")
+    stack1.addinstack("8")
     stack1.display()
     stack1.peek()
+    stack1.pop()
+    print("updated")
+    stack1.display()
