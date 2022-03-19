e276ccca12d749d10b570a1aa37873f78c82bb0b
Shakti09-Rookie <shaktidagar09@gmail.com>
2022-03-19 07:38:08 +0530

    two sums

---
+++ leetcode/Two Sum.py
@@ -0,0 +1,15 @@
+from typing import List
+
+    # def twoSum(self, nums: List[int], target: int) -> List[int]:
+def twoSum(nums: List[int], target: int) -> List[int]:
+    size = len(nums)
+    for i in range(size):
+        for j in range(1+i,size):
+            if(nums[i]+nums[j] == target):
+                print("woow")
+                return [i,j]
+
+
+nums = [3,2,4]
+target = 6
+print(twoSum(nums, target))
