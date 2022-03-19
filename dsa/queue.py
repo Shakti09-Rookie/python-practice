10689709b39572c9966f84c3edc015dbf1357839
Shakti09-Rookie <shaktidagar09@gmail.com>
2022-03-19 06:21:35 +0530

    final stack

--- dsa/queue.py
+++ dsa/queue.py
@@ -5,7 +5,6 @@ class queue:
         self.rear = capacity-1
         self.capacity = capacity
     #enqueue
-    # help taken from GFG
     def eq(self, value):
         if self.isFull():
             print("Queue is Full.")
@@ -14,7 +13,6 @@ class queue:
             self.rear = (self.rear + 1)%(self.capacity)
             self.queue[self.rear] = value
             self.size += 1
-            print(f"size is {self.size}")
             # self.queue.append(value)
     #dequeue
     def dq(self):
@@ -22,8 +20,9 @@ class queue:
             print("Queue is Empty")
             return
         else:
-            self.front = (self.front + 1) % self.capacity
+            self.front = (self.front + 1) % (self.capacity)
             self.size -=1
+            return self.queue.pop(0)
 
     # def fo(self):
     #     self.queue.pop(0)
@@ -32,15 +31,15 @@ class queue:
         if self.isEmpty():
             print("Empty Queue")
 
-        print(self.queue[0])
-        #print(f"value at queue front -> {self.queue[self.front]}")
+        # print(self.queue[0])
+        print(f"value at queue front -> {self.queue[self.front]}")
 
     def q_rear(self):
         if self.isEmpty():
             print("Empty Queue")
 
-        # print(f"value at queue rear -> {self.queue[self.rear]}")
-        print(self.queue[-1])
+        print(f"value at queue rear -> {self.queue[self.rear]}")
+        # print(self.queue[-1])
     #isfull
     def isFull(self):
         return self.size == self.capacity
@@ -57,7 +56,7 @@ if __name__ == "__main__":
     for i in range(sizee):
         queue1.eq(input())
 
-    print(f"is the queue full : ->{queue1.isFull()}")
-    queue1.q_front()
-    queue1.q_rear()
+    # print(f"is the queue full : ->{queue1.isFull()}")
+    queue1.printt()
+    print(f"Dequeued ->  {queue1.dq()}")
     queue1.printt()
