e9654b5b11944dbc6a696c3d60d70a8f92362d2d
Shakti09-Rookie <shaktidagar09@gmail.com>
2022-03-24 09:48:39 +0530

    pallindrome

---
+++ leetcode/pallindrome_number.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isPalindrome(self, x: int):
+        if x<0:
+            print("not a pallindrome")
+        else:
+            reverse = 0
+            x1=x
+            while x1 > 0:
+                digit = x1%10
+                reverse = (reverse*10)+digit
+                x1=x1//10
+
+            if x == reverse:
+                print("pallindrome")
+                return
+            print("not a pallindrome")
+
+obj1 = Solution()
+obj1.isPalindrome(123321)
