ecdb30551fb15bfb62e5f392cd46b982ca8814b3
Shakti09-Rookie <shaktidagar09@gmail.com>
2022-03-26 01:14:42 +0530

    vaild parentheses done

--- leetcode/validparentheses.py
+++ leetcode/validparentheses.py
@@ -1,31 +1,22 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         li = []
-        #grp = {')' : '(' , ']' : '[' , '}' : '{'}
+        pair = {')' : '(' , ']' : '[' , '}' : '{'}
         for i in s:
             if i in (["(","[","{"]):
-                li.append(i)
-                print(li)
-            if i in ([")","]","}"]):
-                print("entered ->", i)
-                pair = {")": "(", "]": "[", "}":"{"}
-                print(li)
-                print("pair of what i want", pair[i])
+                li.append(i)    
+            elif i in ([")","]","}"]):
                 if not li:
-                    print("entered here", li)
                     return False
                 elif li.pop() != pair[i]:
-                    print("entered in pop")
                     return False
                 else:
-                    print("only in else", li)
                     continue
         if not li:
             return True
         else:
             return False
 
-s = "{[()]}{]{}}"
+s = "{[()]}{[[]]{}}"
 obj1 = Solution()
-ans = obj1.isValid(s)
-print(ans)
+print(obj1.isValid(s))
