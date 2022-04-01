c5e5eb1aea5d2cbeb18d3d357dc52bde708e749e
Shakti09-Rookie <shaktidagar09@gmail.com>
2022-04-01 23:51:54 +0530

    addded comment

--- leetcode/Removeduplicates.py
+++ leetcode/Removeduplicates.py
@@ -1,10 +1,13 @@
+from typing import Optional
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        #head can be None(one if the cases)
         if head is None:
             return head
         temp = head
@@ -16,21 +19,15 @@ class Solution:
                 temp = head
                 nextval = temp.next
                 preval = head
-                # print("finished")
                 print(head)
             elif temp.val == nextval.val:
-                # print(temp.val)
-                # print("tnv", nextval.val)
                 preval.next = nextval
                 temp = temp.next
                 nextval = temp.next
-                # nextval = nextval.next.next
             else:
                 print("entered here")
                 preval = temp
                 temp = temp.next
                 nextval = nextval.next
-                # print(temp)
-                # print(preval)
-                # print(nextval)
+
         return head
