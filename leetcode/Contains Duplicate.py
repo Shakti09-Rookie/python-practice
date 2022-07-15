02ebad0936ddc7266d238223578af5bd789afddd
Shakti09-Rookie <shaktidagar09@gmail.com>
2022-07-15 23:39:35 +0530

    217. Contains Duplicate

---
+++ leetcode/Contains Duplicate.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/contains-duplicate/
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        s = set()
+        for num in nums:
+            s.add(num)
+        return len(s) != len(nums)
